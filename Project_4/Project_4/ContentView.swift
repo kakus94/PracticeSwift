2314c717db0a95acfe59d7fc6500acc40374c89f
Kamil <kkarpiak94@icloud.com>
2022-05-25 17:26:15 +0200

    init

---
+++ Project_4/Project_4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_4
+//
+//  Created by Kamil Karpiak on 25/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_4/Project_4/Project_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4App.swift
+//  Project_4
+//
+//  Created by Kamil Karpiak on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
